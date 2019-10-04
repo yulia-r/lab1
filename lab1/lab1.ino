94fc8a8712963d123f0bfe8b190e8fc8745a8d17
Prandtl <prandtl211@gmail.com>
2019-10-04 21:22:39 +0500

    fate motif

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,12 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
 #include "pitches.h"
 #include "button.h"
 #include "buzzer.h"
 
+#define PIN_BUZZER 6
+#define PIN_BUTTON_OFF 5
+
+Button buttonOff(PIN_BUTTON_OFF);
+Buzzer buzzer(PIN_BUZZER);
+
+
+int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};
+double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
+int melodyLength = 8;
+
+void setup() {
+    buzzer.setMelody(notes, durations, melodyLength);
+    buzzer.turnSoundOn();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+    buzzer.playSound();
+    if (buttonOff.wasPressed())
+    {
+        buzzer.turnSoundOff();
+    }
 }
